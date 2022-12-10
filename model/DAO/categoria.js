1d021b05e26876c3234855a14bcf14b93cca16d7
marina.12soft@gmail.com <marina.12soft@gmail.com>
2022-12-10 14:30:50 -0300

    atualizando o repositorio gt

--- model/DAO/categoria.js
+++ model/DAO/categoria.js
@@ -16,7 +16,7 @@ const insertCategoria = async function(categoria) {
 
     try {
 
-        let sql = `insert into tbl_categoria(nome) values('${categoria.nome}')`;
+        let sql = `insert into tbl_categoria(nome, codigo_tipo) values('${categoria.nome}', ${categoria.codigo_tipo})`;
 
         //executa o script SQL no BD ($executeRawUnsafe() permite encaminhar uma variável contendo o script)
         const result = await prisma.$executeRawUnsafe(sql);
